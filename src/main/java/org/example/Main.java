7f226ffc61a5fae683dbe515e5108434bb365fd5
Alina Kornilova <ak1.0@yandex.ru>
2024-11-08 00:40:59 +0300

    First task

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,19 @@
-
 package org.example;
 
+import org.example.task1.BankAccount;
+
+/**
+ * Задача "Банковский счет":
+ * Создайте класс BankAccount c полями для хранения номера счета и баланса.
+ * Реализуйте методы для получения (get), установки (set) баланса, а так же для внесения и снятия денег.
+ * Создайте интерфейс Printable с методом print(), который будет реализован в классе BankAccount для вывода информации
+ */
+
+// комментарий к коду
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        sum();
-    }
-    public static void sum() {
-        int sum = 5 + 1;
-        System.out.println(sum);
+        BankAccount account1 = new BankAccount();
+        account1.setBalance(8000);
+        account1.print();
     }
 }
